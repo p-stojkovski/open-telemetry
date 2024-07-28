eb3a94de1a4746ba2f78c33d594ae81e103f08d5
Petar Stojkovski <petars089@gmail.com>
2024-07-28 12:25:29 +0200

    Added instrumentation for the console tool

---
+++ src/ConsoleTool/ApplicationDiagnostics.cs
@@ -0,0 +1,8 @@
+﻿using System.Diagnostics;
+
+public class ApplicationDiagnostics
+{
+    public const string ActivitySourceName = "Console.Tool.Diagnosticts";
+
+    public static readonly ActivitySource ActivitySource = new(ActivitySourceName);
+}
